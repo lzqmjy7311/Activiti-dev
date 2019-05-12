53242abde025356c2b775c7f311ec115838af1d9
lzqmjy7311 <725207594@qq.com>
2019-05-12 18:22:45 +0800

    工作流入门第一篇

--- src/main/java/com/gbicc/activiti/DomeMain.java
+++ src/main/java/com/gbicc/activiti/DomeMain.java
@@ -85,6 +85,11 @@ public class DomeMain {
                     } else if (DateFormType.class.isInstance(formProperty.getType())) {
                         LOGGER.info("请输入 {} ? 格式：(yyyy-MM-dd)", formProperty.getName());
                         line = scanner.nextLine();
+                        while (!isValidDate(line)) {
+                            LOGGER.info("您的输入有误，请按照要求输入！[{}]", line);
+                            LOGGER.info("请输入 {} ? 格式：(yyyy-MM-dd)", formProperty.getName());
+                            line = scanner.nextLine();
+                        }
                         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
                         Date date = simpleDateFormat.parse(line);
                         map.put(formProperty.getId(), date);
@@ -104,5 +109,22 @@ public class DomeMain {
             }
         }
         LOGGER.info("结束程序");
+    }
+
+    private static boolean isValidDate(String str) {
+        boolean convertSuccess = true;
+        // 指定日期格式为四位年/两位月份/两位日期，注意yyyy/MM/dd区分大小写；
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            // 设置lenient为false.
+            // 否则SimpleDateFormat会比较宽松地验证日期，比如2007/02/29会被接受，并转换成2007/03/01
+            format.setLenient(false);
+            format.parse(str);
+        } catch (ParseException e) {
+            // e.printStackTrace();
+            // 如果throw java.text.ParseException或者NullPointerException，就说明格式不对
+            convertSuccess = false;
+        }
+        return convertSuccess;
     }
 }
