08113bfdc0eca9395b06fa2ec0528c7d7c9b8a93
lzqmjy7311 <725207594@qq.com>
2019-05-13 13:26:29 +0800

    ....

--- src/main/java/com/gbicc/activiti/DomeMain.java
+++ src/main/java/com/gbicc/activiti/DomeMain.java
@@ -13,8 +13,6 @@ import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -113,16 +111,12 @@ public class DomeMain {
 
     private static boolean isValidDate(String str) {
         boolean convertSuccess = true;
-        // 指定日期格式为四位年/两位月份/两位日期，注意yyyy/MM/dd区分大小写；
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
         try {
-            // 设置lenient为false.
-            // 否则SimpleDateFormat会比较宽松地验证日期，比如2007/02/29会被接受，并转换成2007/03/01
             format.setLenient(false);
             format.parse(str);
         } catch (ParseException e) {
-            // e.printStackTrace();
-            // 如果throw java.text.ParseException或者NullPointerException，就说明格式不对
+            // 格式不对
             convertSuccess = false;
         }
         return convertSuccess;
